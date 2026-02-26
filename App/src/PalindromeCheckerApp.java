39f4b1587d67dc9efafa307fef1d23cc4e34b984
yaswanth-raju-arch <yr1479@srmist.edu.in>
2026-02-26 09:38:19 +0530

    yaswanth uc2

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrome Checker app Management System");
+        System.out.println("modify this logic to usecase 2");
+
+    }
+}
